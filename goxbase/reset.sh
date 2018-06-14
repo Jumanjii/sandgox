d20ea5910bf8ba6979e7152300bef8a28137e85d
Allan Jacquet-Cretides <allan.jacquet@qonto.eu>
2018-06-14 10:21:48 +0200

    Add LICENSE and README in file to not delete

--- goxbase/reset.sh
+++ goxbase/reset.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-rm -r $(ls | grep -v Makefile | grep -v goxbase | grep -v .env);
+rm -r $(ls | grep -v Makefile | grep -v goxbase | grep -v .env | grep -v LICENSE | grep -v README.md);
 cp goxbase/main.go .
