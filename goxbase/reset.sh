bf2381230d92ac79f41cb112f850da2ebcbea39b
Allan Jacquet-Cretides <Jumanjii@users.noreply.github.com>
2018-06-14 10:10:18 +0200

    Delete annotation of a list. Will add it later.

--- goxbase/reset.sh
+++ goxbase/reset.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
-# list =""
-# except="Makefile goxbase .env"
-# cd ..;
-# el in $except do $list="$list | $el
 rm -r $(ls | grep -v Makefile | grep -v goxbase | grep -v .env);
 cp goxbase/main.go .
